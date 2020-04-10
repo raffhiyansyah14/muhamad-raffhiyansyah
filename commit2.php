b603e9b5af70b85f78a8426c440dc4e11ca8417b
raffhiyansyah14 <63452064+raffhiyansyah14@users.noreply.github.com>
2020-04-10 18:06:03 +0700

    Create commit2.php

---
+++ commit2.php
@@ -0,0 +1,8 @@
+echo "nama: muhamad raffhiyansyah";
+echo "kelass: XI RPL 2 ";
+echo "sekolah: smk taruna bhakti";
+echo "alamat: gg.keranji";
+echo "no hp: 089516542784";
+echo "email: raffhiyansyazzz@gmail.com"
+?>
+
